45b901218ef6351c9510108e2cdefbf0dfc53777
puck <36668@ma-web.nl>
2023-11-21 12:29:32 +0100

    edit

--- 01A/app.js
+++ 01A/app.js
@@ -3,9 +3,27 @@ class App
     runApplication()
     {
         console.log("hello world!")
+        
+        let eenVariableNaam = "hello"
+        let appNaam = "subway surfers"
+        let versienummer = "0.2"
+        let versiedatum = "21-11-2023"
+        let autheur = "puck"
+        let copyright = "puck"
+        let distributeur = "puck"
+        let darkmode = "false"
+
+        console.log(appNaam)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log
 
     }
 }
 
 let app = new App();
 app.runApplication();
+
+
