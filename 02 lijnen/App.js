818f2a8330a194e4a9c00e88176aefaf35deddc5
puck <36668@ma-web.nl>
2023-11-23 13:01:55 +0100

    edit

--- 02 lijnen/App.js
+++ 02 lijnen/App.js
@@ -5,16 +5,17 @@ class App
         console.log("hello world!");
         let canvas=document.getElementById("canvasId");
         let g = canvas.getContext("2d");
-       
-        g.fillStyle = "#66CDAA";
-        g.fillRect(0,0,canvas.width,canvas.height);
-       
-        g.fillStyle = "#7FFFD4";
-        g.fillRect(0,50,10,10);
         console.log(canvas)
+        g.beginPath()
+        g.fillStyle = "#DA70D6";
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.lineTo(580,20);
+        g.closePath();
+        g.stroke();
+        g.fill()
     }
 }
 
 let app = new App();
 app.runApplication();
-
