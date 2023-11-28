c3fb7b4957544d0db9ace4e7b418c3b9169a6143
puck <36668@ma-web.nl>
2023-11-28 12:24:19 +0100

    edit

---
+++ 02 huisje/App.js
@@ -0,0 +1,55 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        let canvas=document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        
+        g.beginPath();
+        g.fillStyle = "red";
+        g.moveTo(600,400);
+        g.lineTo(800,300);
+        g.lineTo(700,200);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.moveTo(300,100);
+        g.lineTo(200,300);
+        g.lineTo(600,400);
+        g.lineTo(700,200)
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.moveTo(600,400);
+        g.lineTo(600,600);
+        g.lineTo(200,500);
+        g.lineTo(200,300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.moveTo(600,400);
+        g.lineTo(800,300);
+        g.lineTo(800,500);
+        g.lineTo(600,600);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        
+
+     
+    }  
+        
+    }
+
+
+let app = new App();
+app.runApplication();
+
