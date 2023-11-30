848e953f9ee3e57dfca2dff319d756eb0fcc337e
puck <36668@ma-web.nl>
2023-11-30 12:15:39 +0100

    add

--- 03 ifelse/App.js
+++ 03 ifelse/App.js
@@ -2,13 +2,30 @@ class App
 {
     runApplication()
     {
-        let title = document.getElementById("newstitle");
+        console.log("hello world");
+
+        let title = document.getElementById("newtitle")
         console.log(title);
-        let headline = document.getElementsByClass("gamenews headline")
-        console.log(headline)
-        let news = document.getAnimationsclass("gamenews")
-        console.log(news)
-    
+
+        let random = Math.random();
+        console.log(random);
+        if(random < 0.2)
+        {
+            title.style.backgroundColor = "#ff0000"
+        }
+        let newsitem1 = document.getElementsByClassName("gamenews")[0];
+        console.log(newsitem1);
+        if(random < 0.2)
+        {
+            newsitem1.style.backgroundColor = "#48D1CC"
+        }
+        
+        let newsitem2 = document.getElementsByClassName("gamenews")[1];
+        console.log(newsitem2);
+        if(random < 0.2)
+        {
+            newsitem2.style.backgroundColor = "#98FB98"
+        }
     }
 }
 
