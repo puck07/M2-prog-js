dd7d787315de086bdcae3ce485d03376d61d459d
puck <36668@ma-web.nl>
2023-12-05 12:27:15 +0100

    super

--- 04 functions/App.js
+++ 04 functions/App.js
@@ -1,3 +1,12 @@
+function argumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+}
+argumentsAreHandy("super shout out");
+argumentsAreHandy("duper shout out");
+argumentsAreHandy("super shout ");
+
 function superMooiGlobalFunction()
 {
     console.log("ik ben global");
