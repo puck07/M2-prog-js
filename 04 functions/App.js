6d52237237f18fe3da2ea9db70a0b016ff65ccc1
puck <36668@ma-web.nl>
2023-12-05 12:54:15 +0100

    add

--- 04 functions/App.js
+++ 04 functions/App.js
@@ -1,3 +1,45 @@
+
+function heeftEenResultaat()
+{
+    let resultaat =1;
+    return resultaat;
+}
+let hetResultaat = heeftEenResultaat();
+console.log(hetResultaat);
+console.log(heeftEenResultaat());
+
+let x =9;
+let a =3;
+let b =4;
+let c =89;
+
+let y = (a*(x*x) )+ (b*x) +c;
+console.log(y);
+
+function ax2bcWiskunde(x,a,b,c)
+{
+    let y = (a*(x*x) )+ (b*x) +c;
+    return y;
+}
+
+let y1 = ax2bcWiskunde(9,3,4,89);
+console.log(y1);
+
+let y2 = ax2bcWiskunde(3,6,5,45);
+console.log(y2);
+
+let y3 = ax2bcWiskunde(7,1,2,13);
+console.log(y3);
+
+
+
+function Wiskunde(u,h,l)
+{
+    let logY = Math.log(l) + h + Math.pow(u,2);
+    console.log(logY);
+}
+let y4 = Wiskunde(9,8,15);
+
 function argumentsAreHandy(shoutout)
 {
     console.log("do you want to give a shoutout?");
